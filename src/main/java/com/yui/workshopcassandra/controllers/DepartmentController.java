0c5e091da8d954baafca4cd4c479b7693046ebb6
rianfernandesno <zrpapz12@gmail.com>
2025-05-27 14:45:34 -0300

    GET department by id

--- src/main/java/com/yui/workshopcassandra/controllers/DepartmentController.java
+++ src/main/java/com/yui/workshopcassandra/controllers/DepartmentController.java
@@ -5,10 +5,12 @@ import com.yui.workshopcassandra.services.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/departments")
@@ -22,5 +24,12 @@ public class DepartmentController {
         List<DepartmentDTO> result = service.findAll();
 
         return  ResponseEntity.ok().body(result);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<DepartmentDTO> findById(@PathVariable UUID id){
+        DepartmentDTO dto = service.findById(id);
+
+        return ResponseEntity.ok().body(dto);
     }
 }
--- src/main/java/com/yui/workshopcassandra/services/DepartmentService.java
+++ src/main/java/com/yui/workshopcassandra/services/DepartmentService.java
@@ -3,10 +3,13 @@ package com.yui.workshopcassandra.services;
 import com.yui.workshopcassandra.model.dto.DepartmentDTO;
 import com.yui.workshopcassandra.model.entities.Department;
 import com.yui.workshopcassandra.repositories.DepartmentRepository;
+import com.yui.workshopcassandra.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class DepartmentService {
@@ -19,5 +22,10 @@ public class DepartmentService {
         return list.stream().map(x -> new DepartmentDTO(x)).toList();
     }
 
+    public DepartmentDTO findById(UUID id){
+        Optional<Department> dep = repository.findById(id);
+        Department result = dep.orElseThrow(()-> new ResourceNotFoundException("Objeto não encontrado"));
 
+        return new DepartmentDTO(result);
+    }
 }
